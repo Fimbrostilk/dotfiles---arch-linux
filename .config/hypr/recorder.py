b20563b91b0ad0b2e461999904ec6de34ce31015
Portuguez Padrón Angel <165244123+Fimbrostilk@users.noreply.github.com>
2025-03-02 22:59:45 -0600

    Add files via upload

---
+++ .config/hypr/recorder.py
@@ -0,0 +1,13 @@
+import psutil
+import os
+
+def find_a_process(name = 'wf-recorder'):
+	for process in psutil.process_iter(attrs = ['name']):
+		if process.info['name'] == name:
+			return True
+	return False
+
+if find_a_process():
+	os.system('pkill -INT wf-recorder')
+else:
+	os.system('wf-recorder -y -f /home/angel/Videos/wf-recorder/recording.mkv')
